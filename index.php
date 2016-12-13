cd93c4e920cf514cff19d05ecad2b462936a9747
Jon Leech <oddhack@sonic.net>
2016-12-13 04:14:35 -0800

    Populate git repository from public and internal SVN repositories

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+$static_title = 'Khronos SYCL Registry';
+$static_breadcrumb = array(
+    '/registry/' => 'Registry',
+    'NOLINK' => 'SYCL Registry'
+);
+include_once("../../assets/static_pages/khr_page_top.php");
+?>
+
+<h1 class="pagetitle">Khronos SYCL&trade; Registry</h1>
+<div id="mainformat">
+
+
+<h1 style="text-align:center"> Khronos SYCL&trade; Registry </h1>
+
+<p> The SYCL registry contains specifications of SYCL, a C++ programming
+    model for <a href="https://www.khronos.org/registry/cl/"> OpenCL
+    </a>. </p>
+
+<h6> SYCL Specification </h6>
+
+<p> The current version of SYCL is SYCL 2.2. Older versions are also linked. </p>
+
+<ul>
+<li> <a href="specs/sycl-2.2.pdf"> SYCL 2.2 Specification </a> (February 15,
+     2016). </li>
+<li> <a href="specs/sycl-1.2.pdf"> SYCL 1.2 Specification </a> (May 8,
+     2015). </li>
+</ul>
+
+<h6> Providing Feedback on the Registry </h6>
+
+<p> Khronos welcomes comments and bug reports. To provide feedback on the
+    SYCL registry itself (such as reporting missing content, bad links,
+    etc.), file an issue in the <a
+    href="https://github.com/KhronosGroup/SYCL-Registry/issues">
+    SYCL-Registry </a> Github project. </p>
+
+<p> For the SYCL specification, file a bug on the <a
+    href="http://www.khronos.org/bugzilla/"> Khronos Bugzilla </a>. Make
+    sure to fill in the &quot;Product&quot; field in the bug entry form as
+    &quot;OpenCL&quot; and the &quot;Component&quot; field as
+    &quot;SYCL&quot;. </p>
+
+<?php include_once("../../assets/static_pages/khr_page_bottom.php"); ?>
+</body>
+</html>
