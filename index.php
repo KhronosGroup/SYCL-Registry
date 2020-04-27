8486cb348e9633ac9779418c742086be997508ed
Ronan Keryell <rkeryell@xilinx.com>
2020-04-27 12:34:41 -0700

    SYCL 1.2.1 specification revision 7
    Take also the opportunity to clean up some links on the HTML landing page.

--- index.php
+++ index.php
@@ -5,17 +5,16 @@ include_once("../../assets/static_pages/khr_page_top.php");
 ?>
 
 <p> The SYCL registry contains specifications of SYCL, a C++ programming
-    model for <a href="https://www.khronos.org/registry/cl/"> OpenCL
-    </a>. </p>
+    model for heterogeneous computing. </p>
 
 <h6> SYCL Specification </h6>
 
 <p> The current version of SYCL is SYCL 1.2.1. Other versions are also linked. </p>
 
 <ul>
-<li> <a href="specs/sycl-1.2.1.pdf"> SYCL 1.2.1 Specification revision 6</a>
-     (November 14, 2019). </li>
-<li> <a href="specs/sycl-1.2.pdf"> SYCL 1.2 Specification </a> (May 8,
+<li> <a href="specs/sycl-1.2.1.pdf"> SYCL 1.2.1 Specification revision 7</a>
+     (April 27, 2020). </li>
+<li> <a href="specs/sycl-1.2.pdf"> SYCL 1.2 Specification (obsolete)</a> (May 8,
      2015). </li>
 <li> A provisional SYCL 2.2 specification was published in February 2016.  That
      specification was an incomplete work in progress, and should be considered
@@ -28,16 +27,16 @@ include_once("../../assets/static_pages/khr_page_top.php");
 <p> Khronos welcomes comments and bug reports. To provide feedback on the
     SYCL registry itself (such as reporting missing content, bad links,
     etc.), file an issue and/or create a pull-request in the <a
-    href="https://github.com/KhronosGroup/SYCL-Registry/">
+    href="https://github.com/KhronosGroup/SYCL-Registry">
     SYCL-Registry</a> Github project. </p>
 
 <p> For the SYCL specification, open an issue and/or make a
     pull-request on <a
-    href="https://github.com/KhronosGroup/SYCL-Docs/">SYCL-Docs</a>.
+    href="https://github.com/KhronosGroup/SYCL-Docs">SYCL-Docs</a>.
 
 <p> For the SYCL conformance test suite, open an issue and/or make a
     pull-request on <a
-    href="https://github.com/KhronosGroup/SYCL-CTS/">SYCL-CTS</a>.
+    href="https://github.com/KhronosGroup/SYCL-CTS">SYCL-CTS</a>.
 
 <?php include_once("../../assets/static_pages/khr_page_bottom.php"); ?>
 </body>
