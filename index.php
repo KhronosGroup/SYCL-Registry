4d1311a56c39ae1f172c78e76d988486011f31e4
Ronan Keryell <rkeryell@xilinx.com>
2020-06-29 17:45:27 -0700

    Mention all the reference cards along the specifications too

--- index.php
+++ index.php
@@ -13,9 +13,17 @@ include_once("../../assets/static_pages/khr_page_top.php");
 
 <ul>
 <li> <a href="specs/sycl-1.2.1.pdf"> SYCL 1.2.1 Specification revision 7</a>
-     (April 27, 2020). </li>
+     (April 27, 2020)
+     <ul>
+     <li> <a href="https://www.khronos.org/files/sycl/sycl-121-reference-card.pdf">
+      SYCL 1.2.1 Reference Card</a> </li>
+     </ul> </li>
 <li> <a href="specs/sycl-1.2.pdf"> SYCL 1.2 Specification (obsolete)</a> (May 8,
-     2015). </li>
+     2015)
+     <ul>
+     <li> <a href="https://www.khronos.org/files/sycl/sycl-12-reference-card.pdf">
+      SYCL 1.2 Reference Card</a> </li>
+     </ul> </li>
 <li> A provisional SYCL 2.2 specification was published in February 2016.  That
      specification was an incomplete work in progress, and should be considered
      deprecated (it is no longer linked here).  The SYCL 1.2.1 specification is
