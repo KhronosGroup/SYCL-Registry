ce6263ea5a39041fbdc00d1cc78701fc1db224bc
Ronan Keryell <rkeryell@xilinx.com>
2023-04-18 14:25:16 -0700

    Update reference to SYCL 2020 rev7

--- index.php
+++ index.php
@@ -13,10 +13,10 @@ include_once("../../assets/static_pages/khr_page_top.php");
 
 <ul>
 <li>
-  SYCL 2020 Specification revision 6
+  SYCL 2020 Specification revision 7
   (<a href="specs/sycl-2020/html/sycl-2020.html">HTML</a>)
   (<a href="specs/sycl-2020/pdf/sycl-2020.pdf">PDF</a>)
-  (November 13, 2022) </li>
+  (April 18, 2023) </li>
 
   <ul>
   <li>
